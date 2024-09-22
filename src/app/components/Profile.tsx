875d6239ab872acbffa8b4217d7d8c8c218341ce
Vinit <singhvinit435@gmail.com>
2024-09-22 14:05:55 +0530

    Animated Text and Drag Profile

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,5 +1,17 @@
+"use client";
+import {motion} from "framer-motion"
+import Image from "next/image";
+import myImage from "@/app/assets/fourth.png"
+
 export function Profile() {
   return (
-    <div className="w-[340px] h-[400px] p-2 rounded-lg border-white border bg-gray-500"></div>
+    <motion.div
+    className="w-[300px] h-[300px] rounded-xl shadow-lg shadow-pink-500 cursor-grab"
+    drag
+    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+    dragElastic={0.2}
+    >
+      <Image src={myImage} alt="image" draggable="false" className="rounded-xl" />
+    </motion.div>
   );
 }
