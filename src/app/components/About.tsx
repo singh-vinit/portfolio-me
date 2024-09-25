703bdfa08ec546f8a874d7c842a8398fbb5a21e4
Vinit <singhvinit435@gmail.com>
2024-09-26 01:08:29 +0530

    about section and svg

---
+++ src/app/components/About.tsx
@@ -0,0 +1,13 @@
+export function About() {
+  return (
+    <div className="border border-white text-white font-medium text-lg text-justify flex justify-between p-10">
+      <p className="text-2xl font-bold">About me</p>
+      <p className="w-[450px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        excepturi repellendus quasi libero inventore optio ex eum, iusto
+        repudiandae molestias adipisci consectetur aspernatur sit esse porro
+        error unde ratione distinctio.
+      </p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { Name } from "@/app/components/Name";
 import { Profile } from "@/app/components/Profile";
+import { About } from "./components/About";
+import { TypescriptIcon } from "./svg/Typescript";
+import { JavascriptIcon } from "./svg/javascript";
+import { ReactIcon } from "./svg/react";
 
 export default function Home() {
   return (
     <>
-      <main className="h-[400px] flex justify-around items-center">
+      <main className="h-[400px] flex justify-around items-center border border-red-500">
         <Name />
         <Profile />
       </main>
+      <About />
+      <TypescriptIcon />
+      <JavascriptIcon />
+      <ReactIcon />
     </>
   );
 }
